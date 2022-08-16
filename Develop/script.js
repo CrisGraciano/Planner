9e84c641099a7096474b41aa6b5ca04d2161c891
CrisGraciano <cristobalgraciano98@gmail.com>
2022-08-16 00:37:39 -0500

    fixed folder issues

---
+++ Develop/script.js
@@ -0,0 +1,16 @@
+// opening the planner up, day is displayed on top
+
+// when user scrolls down, they see time blocks for 9am-5pm
+
+// when user views said blocks, they are color coded to represent past, present or future
+
+// when user clicks time block, they can enter an event
+document.addEventListener("click", enterEvent)
+
+function enterEvent() {
+    document.getElementById("container");
+    
+}
+// when event is saved, it will stay in local storage
+
+// when page is refrshed, saved events stay the same
---
+++ README.md
@@ -0,0 +1,4 @@
+# Work Day Scheduler Starter Code
+
+Began by planning out pseudo code in script file and looking over the module assingment criteria.
+
