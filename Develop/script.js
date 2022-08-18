a6fe69b0cc7d30adc96d8eff92de43e60fe31f57
CrisGraciano <cristobalgraciano98@gmail.com>
2022-08-17 21:20:14 -0500

    time block colors

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,33 @@
 var $currentDay = $("#currentDay");
 var $currentDate = moment().format("dddd, MMMM Do");
 
-// 1. opening the planner up, day is displayed on top
+var $timeBlocks = $(".timeblock");
+
+// 1. opening the planner up, day is displayed on top (done)
 $(document).ready(function() {
     $currentDay.text($currentDate);
 });
 
-// 2. when user scrolls down, they see time blocks for 9am-5pm
+// 2. when user scrolls down, they see time blocks for 9am-5pm (done)
 
 // 3. when user views said blocks, they are color coded to represent past, present or future
+function colorTimeBlocks() {
+    $timeBlocks.each(function() {
+        $thisBlock = $(this);
+        var thisBlockHour = parseInt($thisBlock.attr("hour"));
 
-// 4. when user clicks time block, they can enter an event
+        if (thisBlockHour == currentHour) {
+            $thisBlock.addClass("current").removeClass("past upcoming");
+        }
+        if (thisBlockHour < currentHour) {
+            $thisBlock.addClass("past").removeClass("current upcoming");
+        }
+        if (thisBlockHour > currentHour) {
+            $thisBlock.addClass("upcoming").removeClass("past current")
+        }
+    });
+}
+// 4. when user clicks time block, they can enter an event (done)
 document.addEventListener("click", enterEvent)
 
 function enterEvent() {
@@ -19,4 +36,4 @@ function enterEvent() {
 }
 // 5. when event is saved, it will stay in local storage
 
-// 6.cwhen page is refrshed, saved events stay the same
+// 6.when page is refrshed, saved events stay the same
